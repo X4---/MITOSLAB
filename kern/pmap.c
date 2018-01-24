98d44bf30fb596df1f60c4c2596eb211d1143566
x4 <x4.chen@gmail.com>
2018-01-24 03:27:46 -0800

    Fix mem_check_bugs

--- kern/pmap.c
+++ kern/pmap.c
@@ -665,7 +665,7 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
         {
             goto err;
         }
-        if((*pte & perm) != perm)
+		if( pte == NULL || (*pte & perm) != perm)
         {
             goto err;
         }
